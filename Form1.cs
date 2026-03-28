3ba3519b3d0e2c6d8958549e3c23b9e033eaaacf
m1nca0 <noname890256@gmail.com>
2026-03-28 17:08:59 +0800

    add class Player

--- Form1.cs
+++ Form1.cs
@@ -4,12 +4,17 @@ namespace EventMod
 {
     public partial class Form1 : Form
     {
-        MyRectangle myRect;
         List<BaseObject> objects = new();
+        Player player;
         public Form1()
         {
             InitializeComponent();
-            myRect = new MyRectangle(0, 0, 0);
+
+            player = new Player(pbMain.Width / 2, pbMain.Height / 2, 0);
+
+            objects.Add(player);
+            objects.Add(new MyRectangle(50, 50, 0));
+            objects.Add(new MyRectangle(100, 100, 45));
         }
 
         private void pbMain_Paint(object sender, PaintEventArgs e)
@@ -18,9 +23,11 @@ namespace EventMod
 
             g.Clear(Color.White);
 
-            g.Transform = myRect.GetTransform();
-
-            myRect.Render(g);
+            foreach (var obj in objects)
+            {
+                g.Transform = obj.GetTransform();
+                obj.Render(g);
+            }
         }
     }
 }
